5f2a2fe232a90a41440118498e6ebdb6bf70ee21
Adam Geiger <champtoday@me.com>
2020-01-22 02:38:49 -0500

    reroute to login if no token

--- src/components/HomeOrTutorial.tsx
+++ src/components/HomeOrTutorial.tsx
@@ -4,15 +4,20 @@ import { Redirect } from 'react-router';
 
 interface StateProps {
   hasSeenTutorial: boolean;
+  token?: string;
 }
 
-const HomeOrTutorial: React.FC<StateProps> = ({ hasSeenTutorial }) => {
-  return hasSeenTutorial ? <Redirect to="/tabs/home" /> : <Redirect to="/tutorial" />
+const HomeOrTutorial: React.FC<StateProps> = ({ hasSeenTutorial, token }) => {
+  return hasSeenTutorial ? 
+    (token ? <Redirect to="/tabs/home" /> : <Redirect to="/login" />)
+    : 
+    <Redirect to="/tutorial" />
 };
 
 export default connect<{}, StateProps, {}>({
   mapStateToProps: (state) => ({
-    hasSeenTutorial: state.user.hasSeenTutorial
+    hasSeenTutorial: state.user.hasSeenTutorial,
+    token: state.user.token
   }),
   component: HomeOrTutorial
 });
