8cf87c2f8a87b0d8ce7942c055d5d8fd851d6882
nrgapple <champtoday@me.com>
2020-02-12 15:50:42 -0500

    use different method for birthday

--- src/util/util.ts
+++ src/util/util.ts
@@ -12,12 +12,5 @@ export const getTimestamp = (date: Date) => {
 }
 
 export const calculateAge = (dob: Date) => {
-  var today = new Date();
-  var birthDate = new Date(dob);
-  var age = today.getFullYear() - birthDate.getFullYear();
-  var m = today.getMonth() - birthDate.getMonth();
-  if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-    age = age - 1;
-  }
-  return age;
+  return moment().diff(dob, 'years');
 }
