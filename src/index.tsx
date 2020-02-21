e957ad7db878832c5244dd5fd5441a60750dcd3f
Adam Geiger <champtoday@me.com>
2020-02-20 19:47:08 -0500

    remove bad pwa code

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { setupConfig } from '@ionic/react';
 import * as serviceWorker from './serviceWorker';
-import { defineCustomElements } from '@ionic/pwa-elements'
-
-defineCustomElements(window);
 
 setupConfig({
     swipeBackEnabled: false,
 });
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.register();
