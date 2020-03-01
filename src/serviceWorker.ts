273d868e758336d8aca4eae7c09b28ada96c3fed
Matt <mjruddy94@gmail.com>
2020-03-01 09:02:45 -0500

    remove logs

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -41,8 +41,6 @@ export function register(config?: Config) {
 
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-      console.log(swUrl);
-      console.log(isLocalhost)
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
         checkValidServiceWorker(swUrl, config);
