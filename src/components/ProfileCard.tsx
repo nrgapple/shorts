935c6c98c479e880283b2e6e256b22f1ca3796aa
Adam Geiger <champtoday@me.com>
2020-03-18 22:20:33 -0400

    move things around

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react';
-import { IonCard, IonCardHeader, IonItem, IonCardContent, IonList, IonRow, IonCol, IonButton, IonIcon, IonLabel, IonSkeletonText, IonCardTitle, IonChip, IonSlides, IonSlide } from '@ionic/react';
+import { IonCard, IonCardHeader, IonItem, IonCardContent, IonList, IonRow, IonCol, IonButton, IonIcon, IonLabel, IonSkeletonText, IonCardTitle, IonChip, IonSlides, IonSlide, IonText } from '@ionic/react';
 import { calendar, body, close, heart } from 'ionicons/icons';
 import { Profile } from '../models/Profile';
 import Lightbox from 'react-image-lightbox';
@@ -18,7 +18,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ profile, swiped }) => {
     var age = today.getFullYear() - birthDate.getFullYear();
     var m = today.getMonth() - birthDate.getMonth();
     if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age = age - 1;
+      age = age - 1;
     }
     return age;
   }
@@ -30,8 +30,7 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ profile, swiped }) => {
   useEffect(() => {
     console.log(profile);
     if (profile && slides.current) {
-      (async () => 
-      {
+      (async () => {
         if (!slides.current)
           return;
         await slides.current.update();
@@ -43,104 +42,105 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ profile, swiped }) => {
   return (
     <>
       {
-        profile?
-        <>
-        <IonCol size="12" size-md="6">
-          <ImageCard images={profile.images} areDeletable={false}/>
-        </IonCol>
-        <IonCol size="12" size-md="6">
-          <IonCard className="home-card">
-            <IonCardHeader translucent>
-              <IonCardTitle>
-                {profile.firstName}
-              </IonCardTitle>
-            </IonCardHeader>
-            <IonRow justify-content-center>
-                <IonCol text-center size="6">
-                  <IonButton
-                    fill="solid"
-                    size="small"
-                    color="danger"
-                    expand="block"
-                    onClick={() => swiped(false)}
-                  >
-                    <IonIcon slot="start" icon={close} />
+        profile ?
+          <>
+            <IonCol size="12" size-md="6">
+              <ImageCard images={profile.images} areDeletable={false} />
+            </IonCol>
+            <IonCol size="12" size-md="6">
+              <IonCard className="home-card">
+                <IonItem lines="none">
+                  <IonLabel><h1><span>{profile.firstName}&nbsp;&nbsp;&nbsp;</span >
+                    <span>
+                      <IonChip color="primary" outline>
+                        <IonIcon icon={calendar} />
+                        <IonLabel>
+                          {calculateAge(profile.dob)}
+                        </IonLabel>
+                      </IonChip>
+                      <IonChip color="secondary" outline>
+                        <IonIcon icon={body} />
+                        <IonLabel>
+                          {profile.height}
+                        </IonLabel>
+                      </IonChip>
+                    </span>
+                    </h1>
+                  </IonLabel>
+                </IonItem>
+                
+                <IonRow justify-content-center>
+                  <IonCol text-center size="6">
+                    <IonButton
+                      fill="solid"
+                      size="small"
+                      color="danger"
+                      expand="block"
+                      onClick={() => swiped(false)}
+                    >
+                      <IonIcon slot="start" icon={close} />
                     Pass
                 </IonButton>
-                </IonCol>
-                <IonCol text-center size="6">
-                  <IonButton 
-                    fill="solid" 
-                    size="small" 
-                    color="success"
-                    expand="block"
-                    onClick={() => swiped(true)}>
-                    <IonIcon slot="start" icon={heart} />
+                  </IonCol>
+                  <IonCol text-center size="6">
+                    <IonButton
+                      fill="solid"
+                      size="small"
+                      color="success"
+                      expand="block"
+                      onClick={() => swiped(true)}>
+                      <IonIcon slot="start" icon={heart} />
                     Like
                 </IonButton>
-                </IonCol>
-                <IonCol>
-                  <IonButton expand="block" routerLink={`/more/${profile.userId}`} >
-                    Profile
+                  </IonCol>
+                  <IonCol>
+                    <IonButton expand="block" routerLink={`/more/${profile.userId}`} >
+                      Profile
                   </IonButton>
-                </IonCol>
-              </IonRow>
-            <IonCardContent class="outer-content">
-              <IonChip color="primary" outline>
-                <IonIcon icon={calendar} />
-                <IonLabel>
-                  {calculateAge(profile.dob)}
-                </IonLabel>
-              </IonChip>
-              <IonChip color="secondary" outline>
-                <IonIcon icon={body} />
-                <IonLabel>
-                {profile.height}
-                </IonLabel>
-              </IonChip>
-            </IonCardContent>
-            <IonCardContent>
-              <p className="ion-padding-start ion-padding-end">
-                { profile.about? profile.about : ''}
-              </p>
-            </IonCardContent>
-          </IonCard>
-        </IonCol>
-        </>
-        :
-        <IonCol>
-          <IonCard className="speaker-card">
-            <IonCardHeader>
-              <IonCardTitle>
-                <IonSkeletonText animated style={{ width: '100%' }}/>
-              </IonCardTitle>
-            </IonCardHeader>
-            <IonSkeletonText animated style={{ width: '100%', height: '40vh'}}/>
-            <IonCardContent class="outer-content">
-              <IonList lines="none">
-                <IonItem>
-                  <IonSkeletonText animated />
-                </IonItem>
-                <IonItem>
-                  <IonSkeletonText animated />
-                </IonItem>
-                <IonItem>
-                  <IonSkeletonText animated />
-                </IonItem>
-              </IonList>
-            </IonCardContent>
-          </IonCard>
-        </IonCol>
+                  </IonCol>
+                </IonRow>
+                <IonCardContent>
+                  <p className="ion-padding-start ion-padding-end">
+                    {profile.about ? profile.about : ''}
+                  </p>
+                </IonCardContent>
+              </IonCard>
+            </IonCol>
+          </>
+          :
+          <IonCol>
+            <IonCard className="speaker-card">
+              <IonCardHeader>
+                <IonCardTitle>
+                  <IonSkeletonText animated style={{ width: '100%' }} />
+                </IonCardTitle>
+              </IonCardHeader>
+              <IonSkeletonText animated style={{ width: '100%', height: '40vh' }} />
+              <IonCardContent class="outer-content">
+                <IonList lines="none">
+                  <IonItem>
+                    <IonSkeletonText animated />
+                  </IonItem>
+                  <IonItem>
+                    <IonSkeletonText animated />
+                  </IonItem>
+                  <IonItem>
+                    <IonSkeletonText animated />
+                  </IonItem>
+                </IonList>
+              </IonCardContent>
+            </IonCard>
+          </IonCol>
       }
       <>
-      {
-        showImage && (
-          <Lightbox
-            mainSrc={bigImage?bigImage:''}
-            onCloseRequest={() => setShowImage(false)}
-          />
-        )
-      }
+        {
+          showImage && (
+            <Lightbox
+              mainSrc={bigImage ? bigImage : ''}
+              onCloseRequest={() => setShowImage(false)}
+            />
+          )
+        }
       </>
     </>
   );
